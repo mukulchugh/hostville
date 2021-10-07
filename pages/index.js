9c1811dc58f1d63958a52569ba3804caa4a215b7
Mukul Chugh <mukulchughofficial@gmail.com>
2021-10-07 21:12:30 +0530

    final

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>HostVille - Housing for You!</title>
+        <title>HostVille - Housing for You</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>Lets Build AirBnb</h1>
     </div>
   );
 }
